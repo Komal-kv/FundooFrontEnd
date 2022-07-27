15d4ff0d5d56910953e30a7a0589322fdc8b35b2
Komal <divyavairagade@gmail.com>
2022-07-27 22:45:26 +0530

    pushed the code for sign in nd forgot email

--- FundooNotes/src/app/app-routing.module.ts
+++ FundooNotes/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotEmailComponent } from './Components/forgot-email/forgot-email.component';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+import { LogInComponent } from './Components/log-in/log-in.component';
+import { RegisterComponent } from './Components/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : 'register', component : RegisterComponent},
+  {path : 'login', component : LogInComponent},
+  {path : 'forgotpassword', component : ForgotPasswordComponent},
+  {path : 'forgotEmail', component : ForgotEmailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FundooNotes/src/app/app.module.ts
+++ FundooNotes/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,20 +13,28 @@ import { ForgotPasswordComponent } from './Components/forgot-password/forgot-pas
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ForgotEmailComponent } from './Components/forgot-email/forgot-email.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
     LogInComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    ForgotEmailComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
